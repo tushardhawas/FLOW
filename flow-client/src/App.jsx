fa051052c1d84682faa1948df652daf9c26c0af6
tushardhawas <tushardhawasssd@gmail.com>
2025-01-22 03:39:27 +0530

    working

--- flow-client/src/App.jsx
+++ flow-client/src/App.jsx
@@ -1,9 +1,18 @@
-import { Button } from "./components/ui/button";
-
+import { Button } from "./components/ui/button"
+import "./App.css"
 function App() {
-  return <>
-    <Button className="m-10">Click me</Button>
-  </>;
+
+
+  return (
+    <>
+    <div className="m-7">ew</div>
+      <Button size="lg" variant="primary" className=" m-2" >Click Me</Button>
+      <Button variant="destructive" className=" m-2" >Click Me</Button>
+      <Button variant="ghost"  className=" m-2" >Click Me</Button>
+      <Button variant="muted" className=" m-2">Click Me</Button>
+      <Button className=" m-2" variant="teritory">Click Me</Button>
+    </> 
+  )
 }
 
-export default App;
+export default App
